ac65952cc27360d3b3380717b380f745e6f3bb31
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-11-20 23:50:17 +0800

    Move UserController logic to StoreUserAction

---
+++ app/Actions/StoreUserAction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Actions;
+
+use GuzzleHttp\Psr7\Request;
+
+class StoreUserAction{
+    public function execute(Request $request): void
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        $user->generateAvatar();
+        $this->dispatch(RegisterUserToNewsLetter::class);
+    }
+}
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,21 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Actions\StoreUserAction;
 use App\Jobs\RegisterUserToNewsLetter;
 use App\Http\Requests\StoreUserRequest;
 
 class UserController extends Controller
 {
-    public function store(StoreUserRequest $request)
+    public function store(StoreUserRequest $request, StoreUserAction $storeUserAction)
     {
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => $request->password,
-        ]);
-
-        $user->generateAvatar();
-        $this->dispatch(RegisterUserToNewsLetter::class);
+        $storeUserAction->execute($request);
 
         return redirect()->route('users.index');
     }
