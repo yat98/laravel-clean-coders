a9048809dbb5975bcfd72cd1a35c1b3b44758e40
Hidayat Chandra <hidayatchandra08@gmail.com>
2022-12-08 17:18:08 +0800

    Allowing lazy loading in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Model::preventlazyLoading();
+        Model::preventlazyLoading(!app()->isProduction());
     }
 }
